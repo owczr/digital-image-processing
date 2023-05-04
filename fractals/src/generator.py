6f1d346b2c8e26760a1bb9535872a0a71e742656
Jakub Owczarek <jakub.owczarek@codete.com>
2023-05-04 18:47:09 +0200

    Split functions and clean code

--- fractals/src/generator.py
+++ fractals/src/generator.py
@@ -8,18 +8,13 @@ MAX_ITERATIONS = 11000
 
 
 def run():
-    pen = turtle.Turtle()
-    pen.speed(0)
-    pen.color("green")
-    pen.penup()
-
+    pen = create_pen()
     affine = AffineTransformation()
-
     coordinates = np.array([0, 0, 1])
+
     for n in range(MAX_ITERATIONS):
         # Calculate new coordinates
         coordinates = np.matmul(coordinates, affine.transformation)
-        print(coordinates)
 
         # Set last element as 1
         coordinates[-1] = 1
@@ -31,7 +26,20 @@ def run():
         x, y, _ = coordinates
 
         # Draw the point
-        pen.goto(4 * 65 * x, 4 * 37 * y - 252)  # scale the fern to fit inside the window
-        pen.pendown()
-        pen.dot(3)
-        pen.penup()
+        draw_point(pen, x, y)
+
+
+def create_pen():
+    pen = turtle.Turtle()
+    pen.speed(0)
+    pen.color("green")
+    pen.penup()
+
+    return pen
+
+
+def draw_point(pen, x, y):
+    pen.goto(4 * 65 * x, 4 * 37 * y - 252)  # scale the fern
+    pen.pendown()
+    pen.dot(3)
+    pen.penup()
