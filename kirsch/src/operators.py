179463822bdc8d20fb08b59566d87533835948db
Jakub Owczarek <jakub.owczarek@codete.com>
2023-05-05 17:57:52 +0200

    Fix kirsch operators

--- kirsch/src/operators.py
+++ kirsch/src/operators.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 class Kirsch:
+    """Calculates all kirsch operators by rotating two matrices"""
     def __int__(self):
         self.__operators = None
 
@@ -11,16 +12,21 @@ class Kirsch:
 
     @operators.getter
     def operators(self):
-        operator = np.array([[-3, -3, 5],
-                             [-3, 0, 5],
-                             [-3, -3, 5]])
+        operator = np.array([[5, 5, 5],
+                             [-3, 0, -3],
+                             [-3, -3, -3]])
+
+        operator45 = np.array([[-3, 5, 5],
+                               [-3, 0, 5],
+                               [-3, -3, -3]])
+
         return [
             operator,
-            np.rot90(operator, 1),
+            operator45,
+            np.rot90(operator),
+            np.rot90(operator45),
             np.rot90(operator, 2),
+            np.rot90(operator45, 2),
             np.rot90(operator, 3),
-            np.fliplr(operator),
-            np.flipud(operator),
-            np.fliplr(np.rot90(operator, 1)),
-            np.flipud(np.rot90(operator, 1)),
+            np.rot90(operator45, 3)
         ]
